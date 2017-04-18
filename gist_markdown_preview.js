386a77f2b7cde91649f909ade4c1140bd9130376
Revath S Kumar <rsk@revathskumar.com>
2017-04-19 00:27:32 +0530

    Full working version without jQuery

--- gist_markdown_preview.js
+++ gist_markdown_preview.js
@@ -1,15 +1,12 @@
 const d = document;
 const qs = d.querySelector.bind(d);
+const qsa = d.querySelectorAll.bind(d);
 const byId = d.getElementById.bind(d);
-let placeHolder;
 
-const togglePreview = holder => {
-  const cqs = qs.bind(holder);
-  debugger;
-  const article = cqs('#js-gist-preview');
-  console.log(article);
+const togglePreview = cqs => {
+  const article = cqs('.js-gist-preview');
   const editor  = cqs('.cm-s-github');
-  const previewBtn = cqs('#js-gist-preview-btn');
+  const previewBtn = cqs('.js-gist-preview-btn');
 
   if (editor.style.display === 'none') {
     editor.style.display = "block";
@@ -20,50 +17,75 @@ const togglePreview = holder => {
     article.style.display = "inline-block";
     previewBtn.innerHTML = "Edit";
   }
-  article.innerHTML = marked(qs(".commit-create > textarea").value, { sanitize: true });
+  article.innerHTML = marked(cqs(".commit-create > textarea").value, { sanitize: true });
 };
 
-const showPreviewButton = (e) => {
-  const isMarkdown = /.*(\.md|\.markdown)$/.test(e.target.value);
-
-  if(!isMarkdown){
-    const previewArea = byId('js-gist-preview');
-    const previewBtn = byId('js-gist-preview-btn');
-
+const renderPreviewArea = (value, cqs) => {
+  const placeHolder = cqs(".commit-create");
+  if (!isMarkdown(value)) {
+    const previewArea = cqs('.js-gist-preview');
     previewArea && previewArea.remove();
-    previewBtn && previewBtn.remove();
-
     placeHolder.style.display = "visible";
     return;
   }
 
-  const previewArea = d.createElement('div');
-  previewArea.id = "js-gist-preview";
-  placeHolder.appendChild(previewArea);
-
-  if(qs('#gist-preview')) {
+  if(cqs('.js-gist-preview')) {
     return;
   }
 
-  const previewBtn = d.createElement('button');
-  previewBtn.type = "button";
-  previewBtn.innerHTML = "Preview";
-  previewBtn.classList.add('btn');
-  previewBtn.id = "js-gist-preview-btn";
+  const previewArea = d.createElement('div');
+  previewArea.classList.add('js-gist-preview');
+  placeHolder.appendChild(previewArea);
+}
 
-  qs(".form-actions").appendChild(previewBtn);
+const isMarkdown = filename => {
+  return /.*(\.md|\.markdown)$/.test(filename);
+}
+
+const renderPreviewButton = (value, cqs) => {
+  if (!isMarkdown(value)) {
+    const previewBtn = cqs('.js-gist-preview-btn');
+    previewBtn && previewBtn.remove();
+    return;
+  }
+
+  if(cqs('.js-gist-preview-btn')) {
+    return;
+  }
+  const previewBtn = createPreviewBtn();
+  cqs('.file-actions').appendChild(previewBtn);
+
   previewBtn.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    d.querySelectorAll(".commit-create").forEach(holder => {
-      console.log(holder);
-      togglePreview(holder);
-    });
+    togglePreview(cqs);
   });
 }
 
+const createPreviewBtn = () => {
+  const previewBtn = d.createElement('button');
+  previewBtn.type = "button";
+  previewBtn.innerHTML = "Preview";
+  previewBtn.classList.add('btn', 'js-gist-preview-btn');
+  return previewBtn;
+}
+
+const setup = gistHolder => {
+  const cqs = gistHolder.querySelector.bind(gistHolder);
+  cqs('.js-gist-filename')
+    .addEventListener('change', e => {
+      renderPreviewButton(e.target.value, cqs);
+      renderPreviewArea(e.target.value, cqs);
+    });
+}
+
 d.addEventListener('DOMContentLoaded', evt => {
-  placeHolder = qs(".commit-create");
-  qs(".js-gist-filename")
-    .addEventListener('change', showPreviewButton);
+  qsa(".js-gist-file").forEach(setup);
+  qs('.js-add-gist-file')
+    .addEventListener('click', e => {
+      setTimeout(() => {
+        const fileList = qsa(".js-gist-file");
+        setup(fileList[fileList.length -1]);
+      }, 100);
+    });
 });
